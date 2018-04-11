008ddae989810ee399ca73f96b46e02c6ac24385
liyuexin <1290821453@qq.com>
2018-04-11 13:57:49 +0800

    test

--- src/main/java/com/light/springboot/controller/PersonController.java
+++ src/main/java/com/light/springboot/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.light.springboot.constant.GenResult;
 import com.light.springboot.service.PersonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +23,9 @@ public class PersonController {
 
     private static Logger logger = LoggerFactory.getLogger(PersonController.class);
 
+    @Value("${server.id}")
+    private String serverId;//读取application.properties文件中的配置
+
     @Resource
     private PersonService personService;
 
@@ -30,6 +34,7 @@ public class PersonController {
     public Map<String, Object> count() {
         int count = personService.count();
         logger.info("person count: " + count);
+        logger.info("server id: " + serverId);
         return GenResult.SUCCESS.genResult(count);
     }
 }
