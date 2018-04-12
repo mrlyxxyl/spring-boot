54925cb6c48b0d1b111cab0275b9c0c347a70447
liyuexin <1290821453@qq.com>
2018-04-12 10:11:29 +0800

    test

---
+++ src/main/java/com/light/springboot/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.light.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User: LiWenC
+ * Date: 18-4-11
+ */
+@RestController
+public class IndexController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "<div style='width:100%; text-align:center;font-size:60px;color:red;'>success!</div>";
+    }
+}
